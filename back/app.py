8ee7334a0ddb7329a76fc0011f179137baff294b
azultasul <azultasul@gmail.com>
2025-03-21 16:15:25 +0900

    aiagent -> langchain 적용

--- back/app.py
+++ back/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 import aiagentModel
-import model_chat
+import model
 from dotenv import load_dotenv
 load_dotenv() 
 
@@ -9,19 +9,19 @@ app = Flask(__name__)
 CORS(app)  # Vite 프론트엔드와 CORS 문제 방지
 
 # chatbot-llm 추가
-@app.route("/api/aiagent", methods=["POST"])
-def aiagent():
-    user_message = request.json["message"]
-    data = aiagentModel.chat(user_message)
-    
-    return jsonify(data)
-
 # @app.route("/api/aiagent", methods=["POST"])
 # def aiagent():
 #     user_message = request.json["message"]
-#     data = model_chat.chat(user_message)
-    
+#     data = aiagentModel.chat(user_message)
+#     
 #     return jsonify(data)
+
+@app.route("/api/aiagent", methods=["POST"])
+def aiagent():
+    user_message = request.json["message"]
+    data = model.chat(user_message)  
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
